fa04bb04e7bc38ded9759280fb819818420dddb2
cho8080 <chn9486@gmail.com>
2025-06-16 16:42:29 +0900

    refactor: 공격 텍스트와 손님의 구조 개선
    공격 텍스트와 손님의 구조 개선

--- Hard_Worker/Assets/Scripts/UI/DamageText.cs
+++ Hard_Worker/Assets/Scripts/UI/DamageText.cs
@@ -7,34 +7,58 @@ using UnityEngine;
 /// 재료 공격 데미지 표시 스크립트입니다.
 /// 활성화시 위로 올라가는 애니메이션을 한 뒤 비활성화됩니다.
 /// </summary>
-public class DamageText : MonoBehaviour
+public class DamageText : MonoBehaviour, IPoolable
 {
+    private Action<GameObject> returnToPool;
+    GameObject damageTexts;
+
     public TMP_Text damageText;
     public RectTransform rect;
-    private Vector2 startPos;
+    public Vector2 startPos = new Vector3(0f, -3f, 0); // 적이 생성될 위치
     public float distance = 15f; // 위로 이동할 거리
     public float duration = 1.5f; // 사라지는데 걸리는 시간
     float speed = 100f;
-
-    void OnEnable()
-    {
-        Init();
-    }
+    public float attackDamage;
 
     /// <summary>
     /// 초기화 스크립트 입니다.
-    /// 위치와 알파값을 초기화합니다.
+    /// 위치와 투명도를 초기화합니다.
     /// </summary>
-    void Init()
+    public void Init(Action<GameObject> returnAction)
     {
-        startPos = rect.anchoredPosition;
-        damageText.color = new Color(damageText.color.r, damageText.color.g, damageText.color.b, 1f);
-    }
+        damageTexts = GameObject.Find("DamageTexts");
+        transform.SetParent(damageTexts.transform);
 
+        // 위치 초기화
+        rect.anchoredPosition = startPos;
+     
+        // 투명도 초기화
+        damageText.color = new Color(damageText.color.r, damageText.color.g, damageText.color.b, 1f);
+
+        returnToPool = returnAction;
+    }
+    /// <summary>
+    /// 생성된 후 실행됩니다.
+    /// </summary>
+    public void OnSpawn()
+    {
+        // 가장 위로 가게 설정
+        damageTexts.transform.SetAsLastSibling();
+        // 위로 올라가는 애니메이션 후, Pool에 return
+        FadeOutAndMove(attackDamage, () =>
+        {
+            // damageTextPool.ReturnObject(0,this.gameObject);
+            OnDespawn();
+        });
+    }
+    public void OnDespawn()
+    {
+        returnToPool?.Invoke(gameObject); // 풀로 반환
+    }
     /// <summary>
     /// 위로 올라가는 애니메이션을 한 뒤 비활성화됩니다.
     /// </summary>
-    public void FadeOutAndMove(int value, Action onComplete)
+    public void FadeOutAndMove(float value, Action onComplete)
     {    
         damageText.DOKill();
         rect.DOKill();
@@ -49,6 +73,8 @@ public class DamageText : MonoBehaviour
         Vector2 randomPos = new Vector2(startPos.x + randomX, startPos.y);
         rect.anchoredPosition = randomPos;
 
+        if (transform == null) { return; }
+           
         // 위로 올라가기
         Vector2 target = randomPos + Vector2.up * distance;
         rect.DOAnchorPos(target, duration);
@@ -59,9 +85,13 @@ public class DamageText : MonoBehaviour
         {
             // 애니메이션 끝난 뒤에 원래 위치로 되돌림
             rect.anchoredPosition = startPos;
-            gameObject.SetActive(false);
 
+            // Pool에 return
             onComplete?.Invoke();
         });
+    }
+    public void ShowText()
+    {
+        damageText.text = attackDamage.ToString();
     }
 }
--- Hard_Worker/Assets/Scripts/UI/DamageTextPool.cs
+++ Hard_Worker/Assets/Scripts/UI/DamageTextPool.cs
@@ -5,60 +5,36 @@ using UnityEngine;
 /// 재료 공격 데미지 텍스트 Pool 스크립트입니다.
 /// 텍스트를 생성하고 Pool을 관리합니다.
 /// </summary>
-public class DamageTextPool : MonoBehaviour
+public class DamageTextPool : ObjectPoolManager
 {
-    public Transform canvasTransform;
-    public Transform damageTexts; // damageText 부모
-    public GameObject damageTextPrefab;
-    public int poolSize = 10; // Pool 사이즈
-    private Queue<DamageText> damageTextPool = new Queue<DamageText>();
+    EnemyManager enemyManager;
 
-    private void Start()
+    protected override void Awake()
     {
-        CreateDamageText();
+        base.Awake(); // 부모의 Awake 실행되도록
+
+        enemyManager = GetComponentInChildren<EnemyManager>();
     }
     /// <summary>
-    /// poolSize의 수만큼 텍스트 생성
+    /// Pool에서 가져오는 기능입니다.
     /// </summary>
-    public void CreateDamageText()
+    /// <returns></returns>
+    public override GameObject GetObject(int prefabIndex)
     {
-        for (int i = 0; i < poolSize; i++)
+        // 적이 생성되고 Spawn 될 수 있게끔 설정
+        if (enemyManager.enemy.gameObject.activeSelf == false
+            || enemyManager.enemy.isAttackEnd == true)
         {
-            InstantiateText();
+            return null;
         }
+        return base.GetObject(prefabIndex);
     }
     /// <summary>
-    /// Pool 확장
+    ///  Pool에서 반납하는 기능입니다.
     /// </summary>
     /// <returns></returns>
-    public DamageText Get()
+    public override void ReturnObject(int prefabIndex, GameObject obj)
     {
-        if (damageTextPool.Count == 0)
-        {
-            // 풀 부족 시 새로 생성
-            InstantiateText();
-        }
-        return damageTextPool.Dequeue();
-    }
-    /// <summary>
-    /// Pool 반납
-    /// </summary>
-    /// <returns></returns>
-    public void Return(DamageText dt)
-    {
-        dt.gameObject.SetActive(false);
-        damageTextPool.Enqueue(dt);
-    }
-    /// <summary>
-    /// 텍스트 생성
-    /// </summary>
-    /// <returns></returns>
-    void InstantiateText()
-    {
-        GameObject obj = Instantiate(damageTextPrefab, canvasTransform);
-        obj.gameObject.SetActive(false);
-        obj.transform.SetParent(damageTexts);
-        
-        damageTextPool.Enqueue(obj.GetComponent<DamageText>());
+      base.ReturnObject(prefabIndex, obj);
     }
 }
