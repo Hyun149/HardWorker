6549260516a23f5e62c73356df458d1e4176e326
lymsg <114712935+lymsg@users.noreply.github.com>
2025-06-16 11:12:06 +0900

    fix: weapon_gamemanager삭제
    씬수정

--- Hard_Worker/Assets/Scripts/Manager/Weapon/WeaponManager.cs
+++ Hard_Worker/Assets/Scripts/Manager/Weapon/WeaponManager.cs
@@ -3,10 +3,21 @@ using UnityEngine;
 
 public class WeaponManager : MonoBehaviour
 {
+    [Header("무기")]
+    public WeaponDataSO defaultWeaponData;  // 기본무기
+
+    [SerializeField] private WeaponInventory weaponInventory;
+    
     private Weapon equippedWeapon;
     [SerializeField] private WeaponStatusUI weaponStatusUI;
     public event Action OnWeaponEquipped;   //착용 이벤트 
-    
+    private void Start()
+    {
+        // 기본 무기 생성 및 등록
+        Weapon defaultWeapon = new Weapon(defaultWeaponData);
+        weaponInventory.AddWeapon(defaultWeaponData);
+        EquipWeapon(defaultWeapon);
+    }
     public void EquipWeapon(Weapon weapon)
     {
         equippedWeapon = weapon;
